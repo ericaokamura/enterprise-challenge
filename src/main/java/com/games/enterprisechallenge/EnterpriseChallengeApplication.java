38dd778dc142e56b01fa054556a2ec7468492da3
ericaokamura <erica.okamura@yahoo.com.br>
2024-05-18 20:22:24 -0300

    first commit

---
+++ src/main/java/com/games/enterprisechallenge/EnterpriseChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.games.enterprisechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseChallengeApplication.class, args);
+	}
+
+}
