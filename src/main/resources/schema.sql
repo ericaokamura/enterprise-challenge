8c07c8eff6bbb93413c82e03d6695f9ff7c1970e
ericaokamura <erica.okamura@yahoo.com.br>
2024-05-19 13:41:21 -0300

    alterando o script sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,6 @@ create database games_db;
 use games_db;
 create table usuario(
     id INT primary key auto_increment,
-    nome_usuario VARCHAR(50),
+    nome_usuario VARCHAR(100),
     senha VARCHAR(200)
 );
