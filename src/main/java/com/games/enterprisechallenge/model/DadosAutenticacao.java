e76327e8b9da356a3395e9301efaf118e51f798b
erica.okamura@yahoo.com.br <erica.okamura@yahoo.com.br>
2024-09-06 23:41:48 -0300

    ajustes

--- src/main/java/com/games/enterprisechallenge/model/DadosAutenticacao.java
+++ src/main/java/com/games/enterprisechallenge/model/DadosAutenticacao.java
@@ -1,5 +1,5 @@
 package com.games.enterprisechallenge.model;
 
-public record DadosAutenticacao(String nomeUsuario, String senha, String roleName) {
+public record DadosAutenticacao(String nomeUsuario, String senha) {
 
 }
